fec407627a0df7ff0aae4cbf9dccbf0a4ddb97cb
Robotopkin <maxim2002yarovoi@gmail.com>
2023-03-14 15:23:26 +0700

    new rel3

---
+++ Models/ModelContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SGK.Models;
+
+public partial class ModelContext : DbContext
+{
+    public ModelContext()
+    {
+    }
+
+    public ModelContext(DbContextOptions<ModelContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Belovskayagre> Belovskayagres { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseCollation("USING_NLS_COMP");
+
+        modelBuilder.Entity<Belovskayagre>(entity =>
+        {
+            entity.HasKey(e => e.Time1970).HasName("BELOVSKAYAGRES_PK");
+
+            entity.ToTable("BELOVSKAYAGRES");
+
+            entity.Property(e => e.Time1970)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("TIME1970");
+            entity.Property(e => e.State)
+                .HasPrecision(6)
+                .HasColumnName("STATE");
+            entity.Property(e => e.Val).HasColumnName("VAL");
+        });
+        modelBuilder.HasSequence("LOGMNR_DIDS$");
+        modelBuilder.HasSequence("LOGMNR_EVOLVE_SEQ$");
+        modelBuilder.HasSequence("LOGMNR_SEQ$");
+        modelBuilder.HasSequence("LOGMNR_UIDS$").IsCyclic();
+        modelBuilder.HasSequence("MVIEW$_ADVSEQ_GENERIC");
+        modelBuilder.HasSequence("MVIEW$_ADVSEQ_ID");
+        modelBuilder.HasSequence("ROLLING_EVENT_SEQ$");
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using SGK.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ModelContext>(options
+    => options.UseOracle("Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=SGKDB)));User ID=SYSTEM;Password=F7L-gw21X;Persist Security Info=True;"));
 
 var app = builder.Build();
 
